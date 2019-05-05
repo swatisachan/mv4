8ee0dfaa225283fb8fe9ed731a9db516ecbf7e32
swati <swatiinfosys86@gmail.com>
2019-05-05 09:26:05 -0700

    result added

--- src/test/java/sele1/sample/NewTest2.java
+++ src/test/java/sele1/sample/NewTest2.java
@@ -1,9 +1,12 @@
 package sele1.sample;
 
+import static org.testng.Assert.assertNotEquals;
+
 import org.testng.annotations.Test;
 
 public class NewTest2 {
   @Test
   public void f() {
+	  assertNotEquals("swati", "swati");
   }
 }
