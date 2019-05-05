b2f3a3cbd7adff5970f9e98044245d91f0e68ee1
swati <swatiinfosys86@gmail.com>
2019-05-05 08:52:23 -0700

    added incorrect assert

--- src/test/java/sele1/sample/NewTest4.java
+++ src/test/java/sele1/sample/NewTest4.java
@@ -1,9 +1,13 @@
 package sele1.sample;
 
+import static org.testng.Assert.assertNotEquals;
+
 import org.testng.annotations.Test;
 
 public class NewTest4 {
   @Test
   public void f() {
+  assertNotEquals("swati", "swati");
   }
+  
 }
